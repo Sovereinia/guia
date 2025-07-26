8477a9e066844baebb8d8717b4948dc4b81c02bc
Abhi-Bohora <abhishek.bohora18@gmail.com>
2025-07-26 18:30:52 +0545

    navbar ally

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -15,27 +15,32 @@ const navOptions = computed(() => [
 </script>
 
 <template>
-  <div class="navbar px-8">
+  <nav class="navbar px-8" role="navigation" aria-label="Main navigation">
     <div class="flex-1">
       <a
         href="https://sovereinia.org/"
         class="hover:opacity-80 transition-opacity text-2xl font-bold text-color"
+        aria-label="Sovereinia homepage"
       >
         Sovereinia
       </a>
     </div>
-    <ul class="flex gap-4 items-center">
-      <li v-for="option in navOptions" :key="option.link">
-        <a :href="option.link" class="hover:opacity-80 transition-opacity">
+    <ul class="flex gap-4 items-center" role="menubar">
+      <li v-for="option in navOptions" :key="option.link" role="none">
+        <a 
+          :href="option.link" 
+          class="hover:opacity-80 transition-opacity"
+          role="menuitem"
+        >
           {{ option.name }}
         </a>
       </li>
-      <li>
+      <li role="none">
         <LanguageSwitcher />
       </li>
-      <li>
+      <li role="none">
         <ThemeToggler />
       </li>
     </ul>
-  </div>
+  </nav>
 </template>
